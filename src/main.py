06eb765b4c8eae4752fd019847f2cab67121fec9
student <daryakarchikova09@gmail.com>
2026-09-16 11:06:42 +0300

    Initial commit: project setup and previous homework Add widget module with mask_account_card and get_date functions Fix import names and verify widget functions in main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import masks
+import widget
 
-print(masks.get_mask_card_number("7000792289606361"))
-print(masks.get_mask_account("73654108430135874305"))
+print(widget.mask_account_card("Visa Platinum 7000792289606361"))
+print(widget.mask_account_card("Счет 73654108430135874305"))
+print(widget.get_date("2024-03-11T02:26:18.671407"))
